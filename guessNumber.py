329684b2dd2ca0ddddfb3678686006f5b5fcc0e4
Jake Adams <artchecks@gmail.com>
2020-02-22 12:57:39 -0700

    added robust comments to aid in training a friend in Input Validation

--- guessNumber.py
+++ guessNumber.py
@@ -1,21 +1,39 @@
 import random
 roll = random.randint(1, 100)
 tries = 0
+"""
+    PARAM(Roll): The random number between 1 and 100
+    PARAM(tries): the number of attempts the user has made
+        This function validates the user input ensuring only integers between 1 and 100 are submitted
+        when criteria is met, this function calls Comparator function to compare user input against
+        the random roll. 
+"""
 def guess_rand(roll, tries):
+    # get user's guess between 1 and 100
     guess = input("Guess what the house rolled, between 1 - 100: ")
+    # tries - Count the users attempts at guessing the roll
     tries += 1
+    # if User input is a number
     if guess.isdigit():
+        # Convert user input to a Integer
         guess = int(guess)
+        # If user input is greater than 100 throw error
         if guess > 100:
             print("too high, please input a Number between 1 - 100")
+            # Call function to allow user to re-attempt
             guess_rand(roll, tries)
+        # If user input is less than 0 throw error
         elif guess <= 0:
             print("too low, please input a Number between 1 - 100")
+            # Call function to allow user to re-attempt
             guess_rand(roll, tries)
         else:
+            # if user input meets required criteria call Comparator function - passing users input, the random roll value, and their number of attempts 
             comparator(guess, roll, tries)
+    # If user input is not a number throw error
     else:
         print("please input only Numbers between 1 - 100")
+        # Call function to allow user to re-attempt
         guess_rand(roll, tries)
 
 # comparator function compares users validated integer against random number generated
